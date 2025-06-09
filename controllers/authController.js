6d0b4bcc86ce1aafd538e774b60f9ba8fd1e75dc
Lilu-B <163324306+Lilu-B@users.noreply.github.com>
2025-06-09 16:14:10 +0100

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -104,10 +104,14 @@ const handleLoginUser = async (req, res) => {
         { expiresIn: '30d' }
       );
   
-      res.status(200).json({
-        msg: 'Login successful.',
-        token
-      });
+      res
+          .cookie('token', token, {
+            httpOnly: true,
+            secure: true, 
+            sameSite: 'none', 
+            maxAge: 30 * 24 * 60 * 60 * 1000, 
+          })
+          .json({ msg: 'Login successful.' });
   
     } catch (error) {
       console.error('Login error:', error);
